63ad29877b4adc90a0a76062194e83a2fefc10a9
Nazarii Yovenko <106098466+NazariiYovenko@users.noreply.github.com>
2023-10-07 01:27:55 +0300

    add save to local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,20 @@ import { ContactList } from './ContactList/ContactList';
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (savedContacts) this.setState({ contacts: savedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts)
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+  }
 
   onChangeFilterValue = event => {
     this.setState({
